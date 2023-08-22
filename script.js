ee1eeb16f20591a286b0430400874fc0ba833282
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-08-22 10:27:04 +0100

    Add getplayerchoice function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // console.log("Welcome to A3 Game Application");
 
-const OPTIONS = ["Rock", "Paper", "Scissors"];
+const OPTIONS = ["rock", "paper", "scissors"];
 
 let randomSelection = OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
 
@@ -12,3 +12,16 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+function getPlayerChoice() {
+    let playerChoice = prompt("What's your play? ").toLowerCase();
+
+    if (OPTIONS.includes(playerChoice)) {
+        return playerChoice;
+    } else {
+        console.error("Wrong game option!!!");
+    }
+
+}
+
+getPlayerChoice();
