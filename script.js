5073f1c7d34b9e1680526bb75931e3d9ff9a46b3
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-08-22 10:09:33 +0100

    Add getcomputerchoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Welcome to A3 Game Application");
+// console.log("Welcome to A3 Game Application");
+
+const OPTIONS = ["Rock", "Paper", "Scissors"];
+
+let randomSelection = OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
+
+// console.log(randomSelection);
+
+
+function getComputerChoice() {
+    return randomSelection;
+}
+
+getComputerChoice();
