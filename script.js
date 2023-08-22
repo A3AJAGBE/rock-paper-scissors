625f5a5beae89f940de0edc909020646cd23ee3a
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-08-22 12:43:53 +0100

    Add previous functions to game function and added a while loop

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
 
 const OPTIONS = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    let randomSelection = OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
-    return randomSelection;
-}
-
-function getPlayerChoice() {
-    let playerChoice = prompt("What's your play? ").toLowerCase();
-
-    if (OPTIONS.includes(playerChoice)) {
-        return playerChoice;
-    } else {
-        console.error("Wrong game option!!!");
-    }
-
-}
-
-function gameRound(playerSelection, computerSelection) {
-   
-    console.log(gameInfo);
-
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "Player Wins.";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "Player Wins.";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "Player Wins.";
-    } else if (playerSelection === computerSelection) {
-        return "It's a draw."
-    } else {
-        return "You lose."
-    }
-}
-
-let pSelection = getPlayerChoice();
-let cSelection = getComputerChoice();
-let gameInfo = `You played "${pSelection}" and the computer played "${cSelection}"`;
-
+const PLAY_ROUND = 5;
+let currentRound = 1;
 
 function game() {
-    console.log(gameRound(pSelection, cSelection))
+    function getComputerChoice() {
+    let randomSelection = OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
+    return randomSelection;
+    }
+
+    function getPlayerChoice() {
+        let playerChoice = prompt("What's your play? ").toLowerCase();
+
+        if (OPTIONS.includes(playerChoice)) {
+            return playerChoice;
+        } else {
+            console.error("Wrong game option!!!");
+        }
+
+    }
+
+    function gameRound(playerSelection, computerSelection) {
+    
+        console.log(gameInfo);
+
+        if (playerSelection == "rock" && computerSelection == "scissors") {
+            return "Player Wins.";
+        } else if (playerSelection == "scissors" && computerSelection == "paper") {
+            return "Player Wins.";
+        } else if (playerSelection == "paper" && computerSelection == "rock") {
+            return "Player Wins.";
+        } else if (playerSelection === computerSelection) {
+            return "It's a draw."
+        } else {
+            return "You lose."
+        }
+    }
+
+    let pSelection = getPlayerChoice();
+    let cSelection = getComputerChoice();
+    let gameInfo = `You played "${pSelection}" and the computer played "${cSelection}"`;
+    console.log(gameRound(pSelection, cSelection));
 }
 
-game();
- 
+
+let roundCompleted = false;
+
+while (!(roundCompleted)) {
+    for (currentRound; currentRound <= PLAY_ROUND; currentRound++) {
+        if (currentRound == 5) {
+            console.log("This is the Final round");
+            game();
+            roundCompleted = true;
+        } else {
+            console.log("This is round " + currentRound);
+            game();
+            
+        }
+    }
+}
